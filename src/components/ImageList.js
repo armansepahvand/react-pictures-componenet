cb319b35311f8464a253327dc37cbe33a722289f
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-12 16:36:59 -0700

    image list componenet to iterate over images reeived as prop and pass it on to image card component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
-import React from 'React';
+import React from "react";
+import './ImageList.css';
+import ImageCard from "./ImageCard";
 
-const ImageList = ()=>{
-    return <div> Images</div>
-}
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return <div className="image-list">{images}</div>;
+};
 
 export default ImageList;
